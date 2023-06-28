4fcf423dabf7041b01043b0f22eda896fbcdc51c
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2023-06-28 07:20:14 +0530

    added the functionality of lowercase and clear button

--- src/TextForm.js
+++ src/TextForm.js
@@ -1,38 +1,60 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 const TextForm = (props) => {
-    
 
-    let changeHandling = (event)=>{
+
+    let changeHandling = (event) => {
         settext(event.target.value);
     }
 
-    let upperChange=()=>{
-        let newtext=text.toUpperCase();
+    let upperChange = () => {
+        let newtext = text.toUpperCase();
+        settext(newtext);
+    }
+    let lowCase = () => {
+        let newtext = text.toLowerCase();
+        settext(newtext);
+    }
+    let clear = () => {
+        let newtext = "";
         settext(newtext);
     }
 
     const [text, settext] = useState("Enter the Text");
 
     return (
-        <div>
+        <>
 
-            <h1>{props.heading}</h1>
+            <div className="container">
+                <h1>{props.heading}</h1>
 
-
-            
-          
-            <div className="mb-3">
-                {/* <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
-                <textarea className="form-control" value={text} onChange={changeHandling} id="exampleFormControlTextarea1" rows="3"></textarea>
+                <div className="mb-3">
+                    {/* <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
+                    <textarea className="form-control" value={text} onChange={changeHandling} id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={upperChange}>
+                        Change To upper Case
+                </button>
+                <button className="btn btn-primary mx-2" onClick={lowCase}>
+                        Change To lower Case
+                </button>
+                <button className="btn btn-primary mx-2" onClick={clear}>
+                        Clear the rext
+                </button>
             </div>
-            <button className="btn btn-primary" onClick={upperChange}>
-                Change To upper Case
-            </button>
+
+            <div className="container">
+                <h3>Text  Summary</h3>
+                <p> {text.split(" ").length} words,  {text.length}characters</p>
+                <p> {0.08* text.split(" ").length} Minutes required</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
 
 
+        </>
 
-        </div>
+
     )
 }
 
