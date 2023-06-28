361059eaf6f781afb8eeb07f25c42d214c7fa871
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2023-06-28 05:53:25 +0530

    Learned setState and applied in TextFormm

--- src/TextForm.js
+++ src/TextForm.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const TextForm = (props) => {
+    
+
+    let changeHandling = (event)=>{
+        settext(event.target.value);
+    }
+
+    let upperChange=()=>{
+        let newtext=text.toUpperCase();
+        settext(newtext);
+    }
+
+    const [text, settext] = useState("Enter the Text");
+
     return (
         <div>
 
             <h1>{props.heading}</h1>
 
 
-
+            
           
             <div className="mb-3">
                 {/* <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea className="form-control" value={text} onChange={changeHandling} id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" onClick={upperChange}>
                 Change To upper Case
             </button>
 
